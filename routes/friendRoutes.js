6526eeda6940540f334cfb61e9a97daeb973dfce
Yoshi <s10266031@connect.np.edu.sg>
2025-07-28 06:07:37 +0800

    friends bind to html test

--- routes/friendRoutes.js
+++ routes/friendRoutes.js
@@ -8,5 +8,7 @@ router.get("/requests/incoming/:userId", controller.getIncomingRequests);
 router.get("/requests/outgoing/:userId", controller.getOutgoingRequests);
 router.post("/requests", controller.sendRequest);
 router.put("/requests/:friendshipId", controller.updateStatus);
+router.patch('/requests/:friendshipId', friendController.updateFriendRequestStatus);
+
 
 module.exports = router;
